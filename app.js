c6d4153bd77991cfc292b869dad5a02389a234f2
EmmaOkafor <okaforifebuche26@gmail.com>
2023-08-30 22:37:13 +0100

    Applied dark mode toggle

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const body = document.querySelector("body")
+const moon = document.querySelector("#moon")
+const  hLinks = document.querySelectorAll("#hLink")
 
+// TOGGLE MENU ICON 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("hidden")
     hamburger.classList.toggle("bg-white")
+})
+
+// REMOVE MENU AFTER CLICK 
+hLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle("hidden")
+        hamburger.classList.toggle("bg-white")
+    })
+} )
+
+// TOGGLE DARK MODE 
+moon.addEventListener("click", () => {
+    body.classList.toggle("dark")
+   
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./*.html"],
   theme: {
     extend: {
       screens: {
-        "sm":"480px"
+        sm:"480px",
+        // md:"768",
+        // lg:"1024",
+        // xl:"1280"
       },
       spacing:{
         "big":"48rem"
